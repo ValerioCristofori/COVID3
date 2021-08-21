57a3d475b7225c58ea400901e12de6a4d2b9dd52
ValerioCristofori <cristoforivalerio@gmail.com>
2021-08-21 15:41:09 +0200

    secondo grafico chiuso

--- plots.py
+++ plots.py
@@ -6,7 +6,17 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 
-def buildPlots(df):
+color_grid = "grey"
+color_label = "black"
+
+
+def groupDateForDays(ax, days):
+    # Ogni 7 giorni inserisco la label nell'asse x
+    fmt_half_year = mdates.DayLocator(interval=days)
+    ax.xaxis.set_major_locator(fmt_half_year)
+
+
+def plotNews(df):
 
     # graficare andamento di New Confirmed, New Recovered e New Deaths
     # prendi input del paese da graficare
@@ -23,14 +33,45 @@ def buildPlots(df):
     ax.plot( x, df_country.loc[:, 'New Recovered'], "-b", label="New Recovered")
     ax.plot( x, df_country.loc[:, 'New Deaths'], "-r", label="New Deaths")
 
-    # Ogni 7 giorni inserisco la label nell'asse x
-    fmt_half_year = mdates.DayLocator(interval=7)
-    ax.xaxis.set_major_locator(fmt_half_year)
+    groupDateForDays(ax, 7)
 
     plt.legend()
     plt.title("Andamento di %s " %(country), color="black")
-    plt.grid(color="grey")
-    plt.ylabel("Totale", color="black")
-    plt.xlabel("Data", color="black")
+    plt.grid(color=color_grid)
+    plt.ylabel("Totale", color=color_label)
+    plt.xlabel("Data", color=color_label)
 
     plt.show()
+
+def plotNewDeathsGlobalTrend(df):
+    # gli elementi dell'asse x sono le date
+    for row in range(1, len(df)):
+        if df.iloc[row]["Date"] == df.iloc[0]["Date"]:
+            break
+
+    x = df.loc[0:row-1, 'Date'].tolist()
+
+    y = []
+    for j in range(0, len(x)):
+        tot = 0
+        for i in range(j, len(df), len(x)):
+            if df.iloc[i]['Date'] == x[j]:
+                tot = tot + df.iloc[i]['New Deaths']
+        y.append(tot)
+    fig, ax = plt.subplots()
+
+    ax.plot(x, y, "-b", label="New Deaths")
+    groupDateForDays(ax, 7)
+
+    plt.legend()
+    plt.title("Andamento dei Nuovi Morti globali ", color="black")
+    plt.grid(color=color_grid)
+    plt.ylabel("Totale", color=color_label)
+    plt.xlabel("Data", color=color_label)
+
+    plt.show()
+
+
+def buildPlots(df):
+    plotNews(df)
+    plotNewDeathsGlobalTrend(df)
